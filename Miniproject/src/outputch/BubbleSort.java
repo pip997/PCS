89dd54569c9a823aaeee3453d12e3ae3b12f591d
Henna1911 <livarleth@gmail.com>
2015-12-12 13:15:21 +0100

    bubblesort kindof works?

--- Miniproject/src/outputch/BubbleSort.java
+++ Miniproject/src/outputch/BubbleSort.java
@@ -1,43 +1,54 @@
 package outputch;
 
-public class BubbleSort extends Sorting {
+public class BubbleSort {
 
+	String[] array = {"klo","hjk","hhh","aef","aef"}; //This is where it needs to import the array
+	
 	public BubbleSort() {
-		super("BubbleSort");
+		
 	}
 
-	public BubbleSort(String name) {
-		super(name);
+	public BubbleSort(String[] inputArray) {
+		array = inputArray;
 	}
 	
-	public void bSort(){
+	public void bubbleSort(){
 		int n = array.length;
-		int temp = 0;
+		String temp;
+		int storeResult;
 		
 		for (int i = 0; i < n; i++){
 			boolean noSwap = true;
 			for (int j = 1; j < (n-i); j++){
-				if (array[j-1] > array[j]){
+				storeResult = array[j-1].compareTo(array[j]); 
+		
+				if (storeResult>0){
 					temp = array[j-1];
 					array[j-1] = array[j];
 					array[j] = temp;
-					increaseCounter();
+					
 					noSwap=false;
 				}
-				increaseCounter();
 			}
 			if (noSwap) break;
 		}
 	}
 	
+	 public void printArray(){
+	        System.out.println();
+	        for (int i=0; i<array.length; i++){
+	            System.out.print(array[i]);
+	            System.out.print(" ");
+	        }
+	        System.out.println();
+	    }
 
 	public static void main(String[] args) {
 		BubbleSort bs = new BubbleSort();
-		bs.getUserInput();
+		
+		bs.bubbleSort();
 		bs.printArray();
-		bs.bSort();
-		bs.printArray();
-		bs.printComplexity();
+		
 		
 	}
 }
---
+++ Miniproject/src/outputch/Test.java
@@ -0,0 +1,18 @@
+package outputch;
+
+
+
+public class Test {
+	
+	String[] inputArray = {"fgh","jkj","and","jklo"};
+	
+public static void main(String[] inputArray){
+		
+		BubbleSort bs = new BubbleSort();
+		bs.bubbleSort();
+		bs.printArray();
+		
+		
+		
+	}
+}
