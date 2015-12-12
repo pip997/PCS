392caa29cd3236125075f9ffc4fb69e04ceab3ab
EmilieLind <emilield@outlook.dk>
2015-12-12 13:11:12 +0100

    outputclient

--- Miniproject/src/miniproject/OutputClient.java
+++ Miniproject/src/miniproject/OutputClient.java
@@ -1,14 +1,30 @@
 package miniproject;
 
+import java.io.IOException;
+import java.io.PrintWriter;
 import java.net.*;
-import java.util.List;
+import java.util.*;
 
 public class OutputClient extends Thread {
-
+	private static InetAddress host;
+	private static final int PORT = 1234;
 	private String outMessage;
 	private List<String> wordList;
+	
+	
 
 	public void run(){
+		
+		Socket socket = null;
+		try {
+			socket = new Socket(host, PORT);
+			Scanner input = new Scanner(socket.getInputStream());
+			PrintWriter output = new PrintWriter(socket.getOutputStream(), true);
+		}
+		catch(IOException ioEx) {
+			ioEx.printStackTrace();
+		}
+		
 		outMessage = "";
 		String tempWord = wordList.get(0);
 		int wordCount = 0;
